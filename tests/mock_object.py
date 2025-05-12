cd9dbe2208b27d7124940ac88a5f258bd9bae3b8
Steffen <steffen.haeussler.work@gmail.com>
2025-05-12 12:33:48 +0200

    add neighbors

--- tests/mock_object.py
+++ tests/mock_object.py
@@ -59,3 +59,25 @@ def information_mock_response(*args, **kwargs):
         mock_resp.json.return_value = body
 
     return mock_resp
+
+
+def neighbor_mock_response(*args, **kwargs):
+    mock_resp = Mock()
+    url_called = args[0]
+
+    try:
+        id_from_url = url_called.split("/")[-1]
+    except IndexError:
+        id_from_url = "unknown_id_from_url"
+
+    if id_from_url == "raise_error":
+        mock_resp.status_code = 500
+        mock_resp.raise_for_status.side_effect = httpx.RequestError(
+            "Simulated network problem"
+        )
+    else:
+        mock_resp.status_code = 200
+
+        mock_resp.json.return_value = str(id_from_url) + "_neighbor"
+
+    return mock_resp
