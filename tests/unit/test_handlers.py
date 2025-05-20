15495edbf7a9d82bc3d73349bba7fa2b8b5242f6
Steffen <steffen.haeussler.work@gmail.com>
2025-05-20 22:31:37 +0200

    fixed unit tests

--- tests/unit/test_handlers.py
+++ tests/unit/test_handlers.py
@@ -1,9 +1,12 @@
 from collections import defaultdict
 
+import pytest
+
 from src.agent.adapters.adapter import AbstractAdapter
 from src.agent.adapters.notifications import AbstractNotifications
 from src.agent.bootstrap import bootstrap
 from src.agent.domain import commands
+from src.agent.service_layer.handlers import InvalidQuestion
 
 
 class FakeAdapter(AbstractAdapter):
@@ -15,15 +18,27 @@ class FakeAdapter(AbstractAdapter):
             response = command
         elif isinstance(command, commands.UseTools):
             response = command
+            response.response = "test tools response"
         elif isinstance(command, commands.LLMResponse):
             response = command
-            response.response = "test response"
+            response.response = "test second llm response"
+            response.chain_of_thought = "test chain of thought"
         elif isinstance(command, commands.Enhance):
             response = command
-            response.response = "test response"
+            response.response = "test first llm response"
+            response.chain_of_thought = "test chain of thought"
         elif isinstance(command, commands.Rerank):
             response = command
-            response.response = "test response"
+            response.candidates = [
+                commands.RerankResponse(
+                    question="test question",
+                    text="test text",
+                    score=0.5,
+                    id="test id",
+                    tag="test tag",
+                    name="test name",
+                )
+            ]
         elif isinstance(command, commands.Retrieve):
             response = command
             response.response = "test response"
@@ -53,32 +68,33 @@ class TestAnswer:
         assert agent.q_id == "test_session_id"
         assert agent.question == "test query"
 
-    # def test_answer_invalid_question(self):
-    #     bus = bootstrap_test_app()
+    def test_answer_invalid_question(self):
+        bus = bootstrap_test_app()
 
-    #     with pytest.raises(InvalidQuestion, match="No question asked"):
-    #         bus.handle(commands.Question(None, None))
+        with pytest.raises(InvalidQuestion, match="No question asked"):
+            bus.handle(commands.Question(None, None))
 
-    # def test_for_new_agent(self):
-    #     bus = bootstrap_test_app()
+    def test_for_new_agent(self):
+        bus = bootstrap_test_app()
 
-    #     assert bus.adapter.seen == set()
+        assert bus.adapter.seen == set()
 
-    #     bus.handle(commands.Question("test query", "test_session_id"))
-    #     assert bus.adapter.seen != set()
+        bus.handle(commands.Question("test query", "test_session_id"))
+        assert bus.adapter.seen != set()
 
-    # def test_return_response(self):
-    #     bus = bootstrap_test_app()
-    #     bus.handle(commands.Question("test query", "test_session_id"))
+    def test_return_response(self):
+        bus = bootstrap_test_app()
+        bus.handle(commands.Question("test query", "test_session_id"))
 
-    #     agent = next(iter(bus.adapter.seen))
-    #     assert agent.response.response == "test response"
+        agent = next(iter(bus.adapter.seen))
 
-    # def test_sends_notification(self):
-    #     fake_notifs = FakeNotifications()
-    #     bus = bootstrap(adapter=FakeAdapter(), notifications=fake_notifs)
-    #     bus.handle(commands.Question("test query", "test_session_id"))
+        assert agent.response.response == "test second llm response"
 
-    #     assert fake_notifs.sent["test_session_id"] == [
-    #         "\nQuestion:\ntest query\nResponse:\ntest response",
-    #     ]
+    def test_sends_notification(self):
+        fake_notifs = FakeNotifications()
+        bus = bootstrap(adapter=FakeAdapter(), notifications=fake_notifs)
+        bus.handle(commands.Question("test query", "test_session_id"))
+
+        assert fake_notifs.sent["test_session_id"] == [
+            "\nQuestion:\ntest query\nResponse:\ntest second llm response",
+        ]
