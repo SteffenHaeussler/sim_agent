5ce278b648976e8bd5527157d3a7dd6fcd980aac
Steffen <steffen.haeussler.work@gmail.com>
2025-05-28 14:26:29 +0200

    added evaluation examples

---
+++ tests/evaluation/test_e2e.py
@@ -0,0 +1,51 @@
+import json
+import os
+from pathlib import Path
+from time import sleep
+
+import httpx
+import pytest
+
+from tests.utils import get_fixtures
+
+current_path = Path(__file__).parent
+
+fixtures = get_fixtures(current_path, keys=["examples"])
+results = []
+
+
+class TestE2E:
+    @pytest.mark.parametrize(
+        "fixture_name, fixture",
+        [
+            pytest.param(fixture_name, fixture, id=fixture_name)
+            for fixture_name, fixture in fixtures.items()
+        ],
+    )
+    def test_area(self, fixture_name, fixture):
+        question, expected_response = (
+            fixture["examples"]["question"],
+            fixture["examples"]["response"],
+        )
+
+        params = {"question": question, "session_id": fixture_name}
+
+        response = httpx.get(
+            os.getenv("agent_api_base_url"),
+            params=params,
+            timeout=60,
+        )
+
+        report = {
+            "question": question,
+            "response": response.text,
+            "expected_response": expected_response,
+        }
+        results.append(report)
+
+        with open("report.json", "w") as f:
+            json.dump(results, f)
+
+        sleep(60)
+
+        assert response
