ee3f68e14cb4aaa07a92c5586028ec1e34f100c7
Steffen <steffen.haeussler.work@gmail.com>
2025-06-04 15:50:38 +0200

    first fix for sse

--- src/agent/service_layer/handlers.py
+++ src/agent/service_layer/handlers.py
@@ -49,6 +49,8 @@ def answer(command: commands.Question, adapter: AbstractAdapter) -> None:
         event = agent.evaluation
         agent.events.append(event)
 
+    agent.events.append(events.EndOfEvent(q_id=agent.q_id))
+
     return None
 
 
@@ -71,20 +73,9 @@ def send_response(
         name="send_response handler",
         session_id=event.q_id,
     )
-    if type(event) is events.Evaluation:
-        message = f"\nQuestion:\n{event.question}\nResponse:\n{event.response}\nSummary:\n{event.summary}\nIssues:\n{event.issues}\nPlausibility:\n{event.plausibility}\nFactual Consistency:\n{event.factual_consistency}\nClarity:\n{event.clarity}\nCompleteness:\n{event.completeness}"
-    elif type(event) is events.ResponseWithData:
-        message = f"\nQuestion:\n{event.question}\nResponse:\n{event.response}"
-
-        if event.data and isinstance(event.data, dict):
-            for key, value in event.data.items():
-                message = message + f"\n{key}: {value}"
-
-    if not isinstance(notifications, list | tuple):
-        notifications = [notifications]
 
     for notification in notifications:
-        notification.send(event.q_id, message)
+        notification.send(event.q_id, event)
     return None
 
 
@@ -108,29 +99,19 @@ def send_failure(
         session_id=event.q_id,
     )
 
-    if type(event) is events.FailedRequest:
-        message = f"\nQuestion:\n{event.question}\nException:\n{event.exception}"
-    elif type(event) is events.RejectedRequest:
-        message = (
-            f"\nQuestion:\n{event.question}\n was rejected. Response:\n{event.response}"
-        )
-    elif type(event) is events.RejectedAnswer:
-        message = f"\nAnswer to question:\n{event.question}\n was rejected. Reason:\n{event.rejection}\n Wrong answer:\n{event.response}"
-    else:
-        raise ValueError("Invalid event type")
-
     for notification in notifications:
-        notification.send(event.q_id, message)
+        notification.send(event.q_id, event)
 
     return None
 
 
 EVENT_HANDLERS = {
     events.FailedRequest: [send_failure],
-    events.ResponseWithData: [send_response],
+    events.Response: [send_response],
     events.RejectedRequest: [send_failure],
     events.RejectedAnswer: [send_failure],
     events.Evaluation: [send_response],
+    events.EndOfEvent: [send_response],
 }
 
 COMMAND_HANDLERS = {
