b84e2f01784cf3b081013be0693e7d3170c00c18
Steffen <steffen.haeussler.work@gmail.com>
2025-06-04 16:37:39 +0200

    fixed tests;

--- tests/integration/test_notifications.py
+++ tests/integration/test_notifications.py
@@ -16,85 +16,72 @@ class TestNotification:
     def test_send_cli_notification_called(self):
         bus = bootstrap(
             adapter=AgentAdapter(),
-            notifications=CliNotifications(),
+            notifications=[CliNotifications()],
+        )
+        event = events.Response(
+            question="test_query",
+            response="test_response",
+            q_id="test_session_id",
         )
         with patch.object(CliNotifications, "send", return_value=None) as mock_send:
-            bus.handle(
-                events.Response(
-                    question="test_query",
-                    response="test_response",
-                    q_id="test_session_id",
-                )
-            )
-            mock_send.assert_called_once_with(
-                "test_session_id", "\nQuestion:\ntest_query\nResponse:\ntest_response"
-            )
+            bus.handle(event)
+            mock_send.assert_called_once_with("test_session_id", event)
 
     def test_send_email_notification_called(self):
         bus = bootstrap(
             adapter=AgentAdapter(),
-            notifications=EmailNotifications(),
+            notifications=[EmailNotifications()],
         )
+        event = events.Response(
+            question="test_query",
+            response="test_response",
+            q_id="test_session_id",
+        )
+
         with patch.object(EmailNotifications, "send", return_value=None) as mock_send:
-            bus.handle(
-                events.Response(
-                    question="test_query",
-                    response="test_response",
-                    q_id="test_session_id",
-                )
-            )
-            mock_send.assert_called_once_with(
-                "test_session_id", "\nQuestion:\ntest_query\nResponse:\ntest_response"
-            )
+            bus.handle(event)
+            mock_send.assert_called_once_with("test_session_id", event)
 
     def test_send_slack_notification_called(self):
+        event = events.Response(
+            question="test_query",
+            response="test_response",
+            q_id="test_session_id",
+        )
+
         bus = bootstrap(
             adapter=AgentAdapter(),
-            notifications=SlackNotifications(),
+            notifications=[SlackNotifications()],
         )
         with patch.object(SlackNotifications, "send", return_value=None) as mock_send:
-            bus.handle(
-                events.Response(
-                    question="test_query",
-                    response="test_response",
-                    q_id="test_session_id",
-                )
-            )
-
-        mock_send.assert_called_once_with(
-            "test_session_id", "\nQuestion:\ntest_query\nResponse:\ntest_response"
-        )
+            bus.handle(event)
+            mock_send.assert_called_once_with("test_session_id", event)
 
     def test_send_ws_notification_called(self):
+        event = events.Response(
+            question="test_query",
+            response="test_response",
+            q_id="test_session_id",
+        )
+
         bus = bootstrap(
             adapter=AgentAdapter(),
-            notifications=WSNotifications(),
+            notifications=[WSNotifications()],
         )
         with patch.object(WSNotifications, "send", return_value=None) as mock_send:
-            bus.handle(
-                events.Response(
-                    question="test_query",
-                    response="test_response",
-                    q_id="test_session_id",
-                )
-            )
-            mock_send.assert_called_once_with(
-                "test_session_id", "\nQuestion:\ntest_query\nResponse:\ntest_response"
-            )
+            bus.handle(event)
+            mock_send.assert_called_once_with("test_session_id", event)
 
     def test_send_sse_notification_called(self):
+        event = events.Response(
+            question="test_query",
+            response="test_response",
+            q_id="test_session_id",
+        )
         bus = bootstrap(
             adapter=AgentAdapter(),
-            notifications=SSENotifications(),
+            notifications=[SSENotifications()],
         )
         with patch.object(SSENotifications, "send", return_value=None) as mock_send:
-            bus.handle(
-                events.Response(
-                    question="test_query",
-                    response="test_response",
-                    q_id="test_session_id",
-                )
-            )
-            mock_send.assert_called_once_with(
-                "test_session_id", "\nQuestion:\ntest_query\nResponse:\ntest_response"
-            )
+            bus.handle(event)
+            mock_send.assert_called_once_with("test_session_id", event)
