eabd21a5f8fa7331dcb3988caba14777258429c8
Steffen <steffen.haeussler.work@gmail.com>
2025-07-03 12:05:15 +0200

    added db adapter

--- src/agent/config.py
+++ src/agent/config.py
@@ -164,3 +164,24 @@ def get_slack_config():
     slack_webhook_url = getenv("slack_webhook_url")
 
     return dict(slack_webhook_url=slack_webhook_url)
+
+
+def get_database_config():
+    db_user = getenv("PG_USER")
+    db_password = getenv("PG_PASSWORD")
+    db_host = getenv("PG_HOST")
+    db_port = getenv("PG_PORT")
+    db_name = getenv("PG_NAME")
+
+    database_connection_string = (
+        f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    )
+    database_type = getenv("database_type", "postgres")
+
+    if database_connection_string is None:
+        raise ValueError("database_connection_string not set in environment variables")
+
+    return dict(
+        connection_string=database_connection_string,
+        db_type=database_type,
+    )
